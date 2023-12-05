5b8622b10c35a2aebb57c73a9ad666292a821ad3
manu <manu@DESKTOP-FGJUHHM>
2023-12-05 12:52:13 +0530

    Third Commit Done

--- Java_Practice/src/jp/Print_Message.java
+++ Java_Practice/src/jp/Print_Message.java
@@ -1,5 +1,16 @@
 package jp;
 
 public class Print_Message {
+	
+	public void print_message()
+	{
+		System.out.println("Hello World");
+	}
+	
+	public static void main(String[] args) {
+		
+		Print_Message pm1=new Print_Message();
+		pm1.print_message();
+	}
 
 }
